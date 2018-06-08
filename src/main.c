82dbcd5289633d2e5349fb728a307b64d41c23df
Josh Spann <jspann128@gmail.com>
2018-06-08 13:33:02 -0500

    [+] Code Cleanup & Parameter Filters
    ~ Code cleaned up and modularized
    + Basic show_help()
    + Loops to create arrays of args & opts from parameters
    + Arguments & Options arrays
    
    [jspann]

--- src/main.c
+++ src/main.c
@@ -7,16 +7,13 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-
-int main(int argcount, char **args) {
-//	printf("Time converter\n");
-	printf("\n");
-
+void hours_minutes_to_float(int argcount, char **args)
+{
 	// Hold marker where seconds start //
 	int secondsi = 0;
 
 	// Go through each argument //
-	for (int i=1; i<argcount; i++) {
+	for (int i=0; i<argcount; i++) {
 		// Temp segregated time values for later conversion //
 		char minuteschar[10];
 		char secondschar[10];
@@ -54,6 +51,58 @@ int main(int argcount, char **args) {
 		printf("%s = ", args[i]);
 		printf("%f minutes\n", totaltime);
 	}
+}
+
+void display_help()
+{
+	char *help = 	"timec: The Simple Time Converter\n"
+					"Usage:\n"
+					"\ttimec [<time>...]\n"
+					"\n";
+	printf("%s", help);
+}
+
+int main(int argcount, char **args)
+{
+	//printf("Time converter\n");
+	printf("\n");
+
+	// Hold total number of arguments vs options passed in //
+	int total_args = 0;
+	int total_opts = 0;
+
+	// Get total amount of arguments vs options from parameters //
+	for (int i=1;i<argcount;i++) {
+		if (args[i][0] == '-') {
+			total_opts++;
+			continue;
+		}
+		total_args++;
+	}
+
+	// Hold separate arrays of arguments and options //
+	char *arguments[total_args];
+	char *options[total_opts];
+
+	// Reuse counters to save memory //
+	total_args = 0;
+	total_opts = 0;
+
+	// Split arguments and options to their own arrays //
+	for (int i=1;i<argcount;i++) {
+		// Assign options //
+		if (args[i][0] == '-') {
+			options[total_opts] = args[i];
+			total_opts++;
+			continue;
+		}
+		// Assign arguments //
+		arguments[total_args] = args[i];
+		total_args++;
+	}
+
+	hours_minutes_to_float(total_args, arguments);
+
 	printf("\n");
 
 	return 0;
