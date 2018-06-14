0dc5e50d9a2e93d79256f79bda5b813a5d16aa1f
Josh Spann <jspann128@gmail.com>
2018-06-14 09:31:29 -0500

    [~] Major Refactoring of Conversion Function
    + Function summary comments
    + get_total_number_args_options()
    ~ Moved main code piece into get_total_number_args_options()
    ~ Moved code to convert string arg to ints into fn()
    + numbers_from_argument() to parse char[] to int
    - Redundant code in hours_minutes_to_float() removed
    
    [jspann]

--- src/main.c
+++ src/main.c
@@ -10,6 +10,98 @@
 
 #define VERSION "1.0"
 
+/**
+ * Return [hh,mm] from "hh:mm"
+ **/
+int* numbers_from_argument(char *arg)
+{
+	static int numbers[2] = {0,0};
+
+	// Hold marker where seconds start //
+	int secondsi = 0;
+
+	// Temp segregated time values for later conversion //
+	char minuteschar[10];
+	char secondschar[10];
+
+	// Converted numbers go here //
+	int minutes = 0;
+	int seconds = 0;
+
+	// Iterate through all characters in argument //
+	for (int j=0;j<sizeof(arg)+2;j++) {
+		char argch = arg[j];
+
+		// Find minutes-seconds marker //
+		if (argch == ':') {
+			secondsi = j;
+			continue;
+		}
+
+		// Print out minutes only //
+		if (j > secondsi && secondsi != 0) {
+			secondschar[j-secondsi-1] = argch;
+			continue;
+		}
+		minuteschar[j] = argch;
+	}
+
+	// Convert minutes and seconds placeholders into floats //
+	minutes = strtoumax(minuteschar, NULL, 10);
+	seconds = strtoumax(secondschar, NULL, 10);
+
+	numbers[0] = minutes;
+	numbers[1] = seconds;
+	return numbers;
+}
+//int* numbers_from_argument(char *argument)
+/*
+{
+	static int numbers[2] = {0,0};
+	int secondsi = 0;
+	char minuteschar[10];
+	char secondschar[10];
+
+	
+//
+		// Iterate through all characters in argument //
+		for (int j=0;j<sizeof(argument)+2;j++) {
+			char argch = argument[j];
+
+			// Find minutes-seconds marker //
+			if (argch == ':') {
+				secondsi = j;
+				continue;
+			}
+
+			// Print out minutes only //
+			if (j > secondsi && secondsi != 0) {
+				secondschar[j-secondsi-1] = argch;
+				continue;
+			}
+			minuteschar[j] = argch;
+		}
+//
+	numbers[0] = 0;
+	numbers[1] = 0;
+	return numbers;
+}*/
+
+// TODO convert number_before_colon to 12-hour time and return output string
+char* clock24_to_clock12(char **arguments)
+{
+	for (int i=0; i<sizeof(arguments); i++) {
+		//int colon_numbers[2] = numbers_from_argument(arguments[i]);
+		//int number_before_colon = colon_numbers[0];
+		//int number_after_colon = colon_numbers[1];
+	}
+
+	return arguments[0];
+}
+
+/**
+ * Convert xx:yy to float value of x (converts y to x's)
+ **/
 void hours_minutes_to_float(int argcount, char **args)
 {
 	// Hold marker where seconds start //
@@ -26,27 +118,8 @@ void hours_minutes_to_float(int argcount, char **args)
 		float seconds = 0;
 		float totaltime = 0;
 
-		// Iterate through all characters in argument //
-		for (int j=0;j<sizeof(args[i])+2;j++) {
-			char argch = args[i][j];
-
-			// Find minutes-seconds marker //
-			if (argch == ':') {
-				secondsi = j;
-				continue;
-			}
-
-			// Print out minutes only //
-			if (j > secondsi && secondsi != 0) {
-				secondschar[j-secondsi-1] = argch;
-				continue;
-			}
-			minuteschar[j] = argch;
-		}
-
-		// Convert minutes and seconds placeholders into floats //
-		minutes = strtoumax(minuteschar, NULL, 10);
-		seconds = strtoumax(secondschar, NULL, 10);
+		minutes = numbers_from_argument(args[i])[0];
+		seconds = numbers_from_argument(args[i])[1];
 		totaltime = minutes+(seconds/60);
 
 		// Output converted, total value to user //
@@ -56,6 +129,9 @@ void hours_minutes_to_float(int argcount, char **args)
 	}
 }
 
+/**
+ * Display help messages
+ **/
 void print_help()
 {
 	char *help = 	"timec: The Simple Time Converter v"
@@ -69,6 +145,30 @@ void print_help()
 	printf("%s", help);
 }
 
+/**
+ *  Get total amount of arguments vs options from parameters
+ **/
+int* get_total_number_args_options(int argcount, char **args)
+{
+	static int argsOpts[2] = {0,0};
+	int total_args = 0;
+	int total_opts = 0;
+
+	// Filter number of args from number of opts //
+	for (int i=1;i<argcount;i++) {
+		if (args[i][0] == '-') {
+			total_opts++;
+			continue;
+		}
+		total_args++;
+	}
+
+	argsOpts[0] = total_args;
+	argsOpts[1] = total_opts;
+
+	return argsOpts;
+}
+
 int main(int argcount, char **args)
 {
 	//printf("Time converter\n");
@@ -77,15 +177,11 @@ int main(int argcount, char **args)
 	// Hold total number of arguments vs options passed in //
 	int total_args = 0;
 	int total_opts = 0;
+	int out_format = 24;
 
 	// Get total amount of arguments vs options from parameters //
-	for (int i=1;i<argcount;i++) {
-		if (args[i][0] == '-') {
-			total_opts++;
-			continue;
-		}
-		total_args++;
-	}
+	total_args = get_total_number_args_options(argcount, args)[0];
+	total_opts = get_total_number_args_options(argcount, args)[1];
 
 	// Hold separate arrays of arguments and options //
 	char *arguments[total_args];
@@ -109,15 +205,27 @@ int main(int argcount, char **args)
 	}
 
 	for (int i=0;i<sizeof(options);i++) {
-		if (strcmp(options[i], "-h") || strcmp(options[i], "--help")) {
+		// Display Help //
+		if (strcmp(options[i], "-h") == 0 || strcmp(options[i], "--help") == 0) {
 			print_help();
 			return 0;
 		}
+		// Set 12-hour Output Formatting //
+		if (strcmp(options[i], "-f12") == 0) {
+			out_format = 12;
+		}
 	}
 
-	if (sizeof(arguments) > 0) 
+	// Convert Time if Args Supplied //
+	if (sizeof(arguments) > 0) {
+		if (out_format == 12) {
+			printf("Formatting as 12 hour time...\n");
+			//TODO printf char* output = clock24_to_clock12(arguments);
+		}
 		hours_minutes_to_float(total_args, arguments);
+	}
 
+	// Default to Print Help if No Args/Opts //
 	if (sizeof(arguments) == 0 && sizeof(options) == 0)
 			print_help();
 
